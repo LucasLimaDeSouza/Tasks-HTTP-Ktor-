4fb4131fb6d979f1aa1383c8d36485877c724cea
Lucas Lima de Souza <lukxx23121996@gmail.com>
2024-06-09 20:19:53 -0300

    first

---
+++ src/main/kotlin/com/first/Application.kt
@@ -0,0 +1,13 @@
+package com.first
+
+import com.first.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
